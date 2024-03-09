f51e85b88c744d6d287d5f9516a36c5e99d99f90
la-belle-femme <labellefemme2023@gmail.com>
2024-03-09 10:00:47 -0500

    practiced/introduced if ... else

---
+++ conditionals/or.py
@@ -0,0 +1,43 @@
+# x = int(input("what's x? "))
+# y = int(input("What's y? "))
+
+# # let's change the values of these variables
+# if x < y or x > y:
+#     print("x is not equal to y")
+
+# # elif x > y:
+# #     print("x is greater than y")    
+
+# else:
+#     print("x is equal to y")
+
+
+# could my code be better? 
+
+
+x = int(input("what's x? "))
+y = int(input("What's y? "))
+
+# let's change the values of these variables
+if x != y:
+    print("x is not equal to y")
+
+# elif x > y:
+#     print("x is greater than y")    
+
+else:
+    print("x is equal to y")      
+
+# Another way of doing is :    
+x = int(input("what's x? "))
+y = int(input("What's y? "))
+
+# let's change the values of these variables
+if x == y:
+    print("x is equal to y")
+
+else:
+    print("x is not equal to y") 
+
+
+
