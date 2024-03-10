3a764cded7025328a4f88b34b72d4d9460879b56
la-belle-femme <labellefemme2023@gmail.com>
2024-03-10 11:18:54 -0400

    solved the exersice with concatenate method

---
+++ format.py
@@ -0,0 +1,13 @@
+'''
+You have a variable called hours which equals 24, the number of hours in a day.
+Print There are 24 hours in a day. Make sure to use your variable.
+First, print using commas. Remember that using commas automatically adds spaces!
+Now, print using string concatenation. Remember to cast hours to a string and manually add the spaces!
+'''
+hours = 24
+# print("There are",hours,"hours in a day.")
+#Code works well but let's concatenate
+
+print(f'There are {hours} hours in a day')
+
+# Let's concatenate, go to concatenate.py file
